5e244c22f1f6ba161daac69c64665590a572ae49
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-16 21:02:15 +0100

    styled components configuration

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import React, {Component} from 'react';
 import styled from 'styled-components/native';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StyledText>Open up App.js to start working on your app!</StyledText>
-      </View>
-    );
-  }
-}
 
 const StyledText = styled.Text`
   color: palevioletred;
 `;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Wrapper = styled.View`
+  flex: 1;
+  background-color: #FFFFFF;
+  align-items: center;
+  justify-content: center;
+`
+
+export default class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <StyledText>Open up App.js to start working on your app!</StyledText>
+      </Wrapper>
+    );
+  }
+}
+
