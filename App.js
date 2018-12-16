f510dc086d3673ee42b842ed9a3c0d2b2d10e91c
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-16 20:53:04 +0100

    styled components

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
+import styled from 'styled-components/native';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StyledText>Open up App.js to start working on your app!</StyledText>
       </View>
     );
   }
 }
+
+const StyledText = styled.Text`
+  color: palevioletred;
+`;
 
 const styles = StyleSheet.create({
   container: {
